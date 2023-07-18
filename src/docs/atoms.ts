8d6102d0a96783f8e41f85ca4aef274e01515bb2
Tim Pietrusky <timpietrusky@gmail.com>
2023-07-18 23:44:16 +0200

    feat: added error handling, show sources, added stop button to cancel generation

--- src/docs/atoms.ts
+++ src/docs/atoms.ts
@@ -1,9 +1,10 @@
 import { atom } from "jotai";
 
 export const answerAtom = atom("");
+export const sourceAtom = atom([]);
 export const tocAtom = atom(false);
 export const questionAtom = atom("");
 export const searchAtom = atom(true);
 export const languageAtom = atom("english");
 export const loadingAtom = atom(false);
-export const scrollSpyAtom = atom("");
+export const errorAtom = atom("");
--- src/docs/components/chat-bot.tsx
+++ src/docs/components/chat-bot.tsx
@@ -1,56 +1,130 @@
-import { Box, Card, CardContent, Checkbox, CircularProgress, Container, Modal } from "@mui/joy";
+import {
+	Alert,
+	Box,
+	Button,
+	List,
+	ListItem,
+	ListItemContent,
+	ListItemDecorator,
+	Sheet,
+	Stack,
+	Typography,
+} from "@mui/joy";
 import axios from "axios";
+import { AxiosError } from "axios";
+
 import { useAtom } from "jotai";
 
-import { answerAtom, languageAtom, loadingAtom, questionAtom } from "@/docs/atoms";
+import {
+	answerAtom,
+	errorAtom,
+	languageAtom,
+	loadingAtom,
+	questionAtom,
+	sourceAtom,
+} from "@/docs/atoms";
 import { ChatInput } from "@/docs/components/chat-input";
 import { LanguageSelect } from "@/docs/components/language-select";
 import { SimpleMarkdown } from "@/docs/components/markdown";
+import ClearIcon from "@mui/icons-material/Clear";
+import { useRef } from "react";
 
 export function Bot({
 	onAnswer,
 	onQuestion,
+	onSource,
 }: {
 	onQuestion?(): void;
 	onAnswer?(data: { id: string; message: Record<string, any> }): void;
+	onSource?(data: { paths: string[] }): void;
 }) {
+	const abortController = useRef<AbortController | null>(null);
+
 	const [loading, setLoading] = useAtom(loadingAtom);
+	const [error, setError] = useAtom(errorAtom);
 
 	const [question] = useAtom(questionAtom);
 	const [language] = useAtom(languageAtom);
 
 	async function handleSubmit() {
+		abortController.current = new AbortController();
 		setLoading(true);
+		setError("");
 
 		if (onQuestion) {
 			onQuestion();
 		}
 
 		try {
-			const { data } = await axios.post("/api/ama", {
-				question,
-				language,
-			});
+			const { data } = await axios.post(
+				"/api/ama",
+				{
+					question,
+					language,
+				},
+				{ signal: abortController.current.signal }
+			);
 
 			if (onAnswer) {
-				onAnswer(data);
+				onAnswer(data.message);
+			}
+
+			if (onSource) {
+				onSource(data.paths);
 			}
 		} catch (error) {
-			console.error(error);
+			const err = error as AxiosError;
+			if (err.message !== "canceled") {
+				console.error(err);
+				setError(
+					err.response?.data?.message ||
+						"There was a problem interacting with the AI, please try again!"
+				);
+			}
 		} finally {
 			setLoading(false);
 		}
 	}
 
+	async function handleCancel() {
+		if (abortController.current) {
+			abortController.current.abort();
+		}
+		setLoading(false);
+	}
+
 	return (
 		<Box>
-			<Box sx={{ maxWidth: { md: 600, lg: 600 }, mx: "auto" }}>
-				<Box component="form" sx={{ p: 1 }} onSubmit={handleSubmit}>
-					<ChatInput loading={loading} onSubmit={handleSubmit} />
-					<Box mt={1} sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-						<LanguageSelect />
+			<Box sx={{ maxWidth: { md: 800, lg: 800 }, mx: "auto" }}>
+				<Stack sx={{ gap: 1 }}>
+					<Box component="form" onSubmit={handleSubmit}>
+						<ChatInput loading={loading} onSubmit={handleSubmit} />
+						<Box mt={1} sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+							<LanguageSelect />
+						</Box>
 					</Box>
-				</Box>
+					{error && <Alert color="danger">{error}</Alert>}
+
+					{loading && (
+						<Box
+							sx={{
+								width: "100%",
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+							}}
+						>
+							<Button
+								startDecorator={<ClearIcon />}
+								onClick={handleCancel}
+								color="neutral"
+								variant="outlined"
+							>
+								Stop generating
+							</Button>
+						</Box>
+					)}
+				</Stack>
 			</Box>
 		</Box>
 	);
@@ -58,6 +132,7 @@ export function Bot({
 
 export default function ChatBot() {
 	const [answer, setAnswer] = useAtom(answerAtom);
+	const [source, setSource] = useAtom(sourceAtom);
 	const [loading] = useAtom(loadingAtom);
 
 	function handleAnswer(data) {
@@ -66,47 +141,52 @@ export default function ChatBot() {
 
 	function handleQuestion() {
 		setAnswer("");
+		setSource([]);
+	}
+
+	function handleSource(data) {
+		setSource(data);
 	}
 
 	return (
-		<Box>
-			<Container sx={{ height: "calc(100% - 6rem)" }}>
-				<Card variant="plain" sx={{ boxShadow: "lg", height: "100%" }}>
-					<Bot onAnswer={handleAnswer} onQuestion={handleQuestion} />
+		<Box sx={{ height: "calc(100% - 6rem)" }}>
+			<Sheet variant="plain" sx={{ height: "100%", p: 2 }}>
+				<Bot onAnswer={handleAnswer} onQuestion={handleQuestion} onSource={handleSource} />
+			</Sheet>
+
+			<Stack sx={{ alignItems: "center" }}>
+				<Box sx={{ maxWidth: { md: 720, lg: 720 } }}>
 					<Box
 						sx={{
 							display: "flex",
+							flexDirection: "column",
 							flex: 1,
-							overflow: "auto",
-							overscrollBehavior: "contain",
+							minHeight: "max-content",
+							p: 2,
 						}}
 					>
-						<CardContent
-							sx={{
-								display: "flex",
-								flexDirection: "column",
-								flex: 1,
-								minHeight: "max-content",
-							}}
-						>
-							{loading ? (
-								<Box
-									sx={{
-										width: "100%",
-										display: "flex",
-										alignItems: "center",
-										justifyContent: "center",
-									}}
-								>
-									<CircularProgress size="lg" />
-								</Box>
-							) : (
+						{!loading && (
+							<Box>
 								<SimpleMarkdown content={answer} />
-							)}
-						</CardContent>
+							</Box>
+						)}
 					</Box>
-				</Card>
-			</Container>
+
+					{source.length > 0 && (
+						<Sheet sx={{ p: 2 }}>
+							<Typography level="h4">Sources</Typography>
+							<List size="sm" sx={{ width: "100%" }}>
+								{source.map((item, index) => (
+									<ListItem key={index}>
+										<ListItemDecorator>📄</ListItemDecorator>
+										{item}
+									</ListItem>
+								))}
+							</List>
+						</Sheet>
+					)}
+				</Box>
+			</Stack>
 		</Box>
 	);
 }
