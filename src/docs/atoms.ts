0a34464ad747c73d03a5f43b773a47fcc1527545
Tim Pietrusky <timpietrusky@gmail.com>
2023-07-18 10:56:55 +0200

    feat: removed unused code

--- src/docs/atoms.ts
+++ src/docs/atoms.ts
@@ -1,12 +1,9 @@
 import { atom } from "jotai";
 
-export const modalAtom = atom(false);
 export const answerAtom = atom("");
-export const drawerAtom = atom(false);
 export const tocAtom = atom(false);
 export const questionAtom = atom("");
 export const searchAtom = atom(true);
-export const guideAtom = atom(false);
 export const languageAtom = atom("english");
 export const loadingAtom = atom(false);
 export const scrollSpyAtom = atom("");
