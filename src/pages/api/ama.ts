1d2eb38823360fade70605d3c3f08f67447e9bca
Tim Pietrusky <timpietrusky@gmail.com>
2023-07-18 23:44:46 +0200

    feat: added error handling, added paths

--- src/pages/api/ama.ts
+++ src/pages/api/ama.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { AxiosError } from "axios";
 
 import { ama } from "@/docs/agents/ama";
 
@@ -9,10 +10,37 @@ export default async function handler(request: NextApiRequest, response: NextApi
 				const { question, language } = request.body;
 				const hyvResponse = await ama({ question, language });
 
-				response.status(200).json(hyvResponse);
+				const message = await hyvResponse.message;
+				const paths = hyvResponse.paths;
+
+				response.status(200).json({ message, paths });
 			} catch (error) {
-				console.log(error);
-				response.status(400).send("Bad Request");
+				const err = error as AxiosError;
+				let errorMessage = "";
+
+				// If we have an error message from the data.error.message, use that
+				if (err.response?.data?.error?.message && err.response.data.error.message !== "") {
+					errorMessage = err.response.data.error.message;
+				}
+				// If there's no message but there's a code, use the code
+				else if (err.response?.data?.error?.code) {
+					errorMessage = err.response.data.error.code;
+				}
+				// If there's neither a message nor a code, use the error's own message
+				else if (err.message) {
+					errorMessage = err.message;
+				} else {
+					errorMessage = "UNKNOWN_ERROR";
+				}
+
+				if (errorMessage.includes("JSON5: invalid character")) {
+					errorMessage =
+						"There was a problem when interacting with the AI, please try again!";
+				}
+
+				console.log(errorMessage);
+
+				response.status(500).json({ message: errorMessage });
 			}
 
 			break;
