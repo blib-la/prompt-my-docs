4e16b9d446b89b66ba9a4d32f97ab03b07fea80a
Tim Pietrusky <timpietrusky@gmail.com>
2023-07-18 10:56:44 +0200

    docs: added docs to make sure it's clear what is happening

--- src/docs/loaders/DirectoryLoaderPro.ts
+++ src/docs/loaders/DirectoryLoaderPro.ts
@@ -1,8 +1,25 @@
 import { DirectoryLoader, UnknownHandling } from "langchain/document_loaders/fs/directory";
 
+/**
+ * `DirectoryLoaderPro` extends the `DirectoryLoader` class from
+ * 'langchain/document_loaders/fs/directory' with additional functionality.
+ *
+ * @property {string[]} ignoredFolders - This is an array of folders which
+ * are to be ignored during the loading process.
+ */
 export class DirectoryLoaderPro extends DirectoryLoader {
 	ignoredFolders: string[];
 
+	/**
+	 * Creates a new instance of the `DirectoryLoaderPro` class, and can specify
+	 * folders to be ignored.
+	 *
+	 * @param {string} directoryPath The path of the directory to load documents from.
+	 * @param {any} loaders The loaders that are used for loading the documents.
+	 * @param {string[]} ignoredFolders An array of folders that are to be ignored during the loading process.
+	 * @param {boolean} recursive A boolean value representing whether sub-directories should be loaded recursively.
+	 * @param {UnknownHandling} unknown An `UnknownHandling` enum value to specify the action to be taken when documents with unknown metadata are encountered.
+	 */
 	constructor(
 		directoryPath: string,
 		loaders: any,
@@ -14,6 +31,11 @@ export class DirectoryLoaderPro extends DirectoryLoader {
 		this.ignoredFolders = ignoredFolders;
 	}
 
+	/**
+	 * Loads the documents from the directory and filters out the ignored folders.
+	 *
+	 * @returns {Promise<any>}
+	 */
 	async load(): Promise<any> {
 		let documents = await super.load();
 
@@ -22,10 +44,6 @@ export class DirectoryLoaderPro extends DirectoryLoader {
 			document =>
 				!this.ignoredFolders.some(folder => document.metadata.source.includes(folder))
 		);
-
-		// documents.map(document => {
-		// 	console.log(document.metadata.source);
-		// });
 
 		return documents;
 	}
--- src/docs/splitter/JavaScriptTextSplitter.ts
+++ src/docs/splitter/JavaScriptTextSplitter.ts
@@ -1,6 +1,10 @@
 import type { RecursiveCharacterTextSplitterParams } from "langchain/text_splitter";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
+/**
+ * `JavaScriptTextSplitter` class is an extension of `RecursiveCharacterTextSplitter`.
+ * The class is primarily used for splitting and handling JavaScript / TypeScript code.
+ */
 export class JavaScriptTextSplitter extends RecursiveCharacterTextSplitter {
 	constructor(fields?: Partial<RecursiveCharacterTextSplitterParams>) {
 		super(fields);
@@ -22,11 +26,25 @@ export class JavaScriptTextSplitter extends RecursiveCharacterTextSplitter {
 		this.separators = fields?.separators ?? separators;
 	}
 
+	/**
+	 * Splits a provided text string into an array of strings, with removed indentation.
+	 *
+	 * @param text - The input text to be split.
+	 *
+	 * @returns A promise which resolves with an array of sub-strings.
+	 */
 	async splitText(text: string): Promise<string[]> {
 		const noIndentText = this.removeIndentation(text);
 		return super.splitText(noIndentText);
 	}
 
+	/**
+	 * Removes leading whitespace from each line in a text.
+	 *
+	 * @param text - The input text from which to remove indentation.
+	 *
+	 * @returns The text with leading whitespace removed from each line.
+	 */
 	private removeIndentation(text: string): string {
 		return text
 			.split("\n")
