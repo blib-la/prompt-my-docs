1a272f2ad4a610cc2960cd0b041a6663e747a959
Tim Pietrusky <timpietrusky@gmail.com>
2023-07-18 10:56:22 +0200

    feat: removed unused code, many simplifications

--- src/docs/components/markdown.tsx
+++ src/docs/components/markdown.tsx
@@ -1,37 +1,7 @@
-import LinkIcon from "@mui/icons-material/Link";
 import { Box, Link, List, ListItem, ListItemDecorator, Typography } from "@mui/joy";
-import slugify from "@sindresorhus/slugify";
-import { useAtom } from "jotai";
-import NextLink from "next/link";
-import { useRef } from "react";
 import ReactMarkdown from "react-markdown";
 import type { Components } from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { scrollSpyAtom } from "@/docs/atoms";
-import { useScrollSpy } from "@/docs/hooks/scroll-spy";
-
-function SpyTypography({ children, ...props }) {
-	const id = slugify(String(children));
-	const ref = useRef<HTMLDivElement>(null);
-	const [, setScrollSpy] = useAtom(scrollSpyAtom);
-	useScrollSpy(ref);
-	return (
-		<Typography ref={ref} id={id} {...props}>
-			{children}
-			<NextLink
-				shallow
-				scroll={false}
-				aria-label={id}
-				href={`#${id}`}
-				onClick={() => {
-					setScrollSpy(id);
-				}}
-			>
-				<LinkIcon sx={{ ml: 2, fontSize: "1rem" }} />
-			</NextLink>
-		</Typography>
-	);
-}
 
 export const simpleComponents = {
 	h1({ children }) {
@@ -131,67 +101,8 @@ export const simpleComponents = {
 	},
 };
 
-export const components = {
-	...simpleComponents,
-	h1({ children }) {
-		return (
-			<SpyTypography level="h1" my={2.5}>
-				{children}
-			</SpyTypography>
-		);
-	},
-	h2({ children }) {
-		return (
-			<SpyTypography level="h2" my={2}>
-				{children}
-			</SpyTypography>
-		);
-	},
-	h3({ children }) {
-		return (
-			<SpyTypography level="h3" my={1.5}>
-				{children}
-			</SpyTypography>
-		);
-	},
-	h4({ children }) {
-		return (
-			<SpyTypography level="h4" my={1}>
-				{children}
-			</SpyTypography>
-		);
-	},
-	h5({ children }) {
-		return (
-			<SpyTypography level="h5" my={1}>
-				{children}
-			</SpyTypography>
-		);
-	},
-	h6({ children }) {
-		return (
-			<SpyTypography level="h6" my={0.5}>
-				{children}
-			</SpyTypography>
-		);
-	},
-};
-
 export interface MarkdownProps {
 	content: string;
-}
-
-export function Markdown({ content }: MarkdownProps) {
-	return (
-		<Box
-			sx={{
-				width: "100%",
-				"> pre": { overflow: "auto" },
-			}}
-		>
-			<ReactMarkdown components={components as Components}>{content}</ReactMarkdown>
-		</Box>
-	);
 }
 
 export function SimpleMarkdown({ content }: MarkdownProps) {
