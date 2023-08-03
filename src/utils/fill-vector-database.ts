bf6d2b4035089d74470a5324520b366336a5ceed
Tim Pietrusky <timpietrusky@gmail.com>
2023-08-03 15:17:45 +0200

    feat: change between different docs and added batch import

--- src/utils/fill-vector-database.ts
+++ src/utils/fill-vector-database.ts
@@ -1,13 +1,20 @@
-import { DOCS, store } from "../docs/weaviate.js";
+import { store } from "../docs/weaviate.js";
 import { Document } from "langchain/document";
 
-export async function fillVectorDatabase(docs: Document<Record<string, any>>[]) {
-	docs.map(async document => {
+export function fillVectorDatabase(docs: Document<Record<string, any>>[], className: string) {
+	let batcher = store.client.batch.objectsBatcher();
+
+	docs.map(document => {
 		const {
 			pageContent,
 			metadata: { source },
 		} = document;
 
-		return store.set({ content: pageContent, path: source }, DOCS);
+		batcher = batcher.withObject({
+			class: className,
+			properties: { content: pageContent, path: source },
+		});
 	});
+
+	return batcher.do();
 }
