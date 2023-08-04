7ebcd0350e1cd4ec98c9328b97c9be3344b59c64
Tim Pietrusky <timpietrusky@gmail.com>
2023-08-04 14:55:25 +0200

    feat: added sx

--- src/docs/components/collapsible-box.tsx
+++ src/docs/components/collapsible-box.tsx
@@ -1,13 +1,15 @@
-import { Box, Typography } from "@mui/joy";
+import { Box, Typography, Sheet } from "@mui/joy";
 import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 import React, { useState, ReactNode } from "react";
+import { SxProps, Theme } from "@mui/material/styles";
 
 interface Props {
 	title: string;
 	children: ReactNode;
+	sx?: SxProps<Theme>;
 }
 
-export default function CollapsibleBox({ title, children }: Props) {
+export default function CollapsibleBox({ title, children, sx = [] }: Props) {
 	const [isVisible, setIsVisible] = useState(false);
 
 	const toggleVisibility = () => {
@@ -15,7 +17,10 @@ export default function CollapsibleBox({ title, children }: Props) {
 	};
 
 	return (
-		<Box>
+		<Sheet
+			variant="outlined"
+			sx={[...(Array.isArray(sx) ? sx : [sx]), { background: "transparent", p: 1 }]}
+		>
 			<Box
 				display="flex"
 				justifyContent="space-between"
@@ -23,10 +28,10 @@ export default function CollapsibleBox({ title, children }: Props) {
 				onClick={toggleVisibility}
 				sx={{ cursor: "pointer" }}
 			>
-				<Typography level="h3">{title}</Typography>
+				<Typography level="h4">{title}</Typography>
 				<div>{isVisible ? <KeyboardArrowUp /> : <KeyboardArrowDown />}</div>
 			</Box>
 			{isVisible && <div>{children}</div>}
-		</Box>
+		</Sheet>
 	);
 }
